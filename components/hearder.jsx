b6dabf097efb6ff0d4569dc7ccc2fa4a4fb00235
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-25 22:36:49 +0530

    upload

--- components/hearder.jsx
+++ components/hearder.jsx
@@ -1,8 +1,21 @@
 import { SignedIn, SignedOut,SignInButton,UserButton } from "@clerk/nextjs"
-
+import Image from "next/image"
+import Link from "next/link"
 const Header = () => {
   return (
-    <div>
+    <header>
+
+      <nav>
+        <Link href ="/">
+         <Image 
+         src="/logo.png" 
+         alt="Sensai Logo"
+          width={200} 
+          height={60} 
+          className="h-12 py-1 w-auto  object-contain"/>
+          
+        </Link>
+      </nav>
 
      <SignedOut>
        <SignInButton/>
@@ -10,8 +23,8 @@ const Header = () => {
      <SignedIn>
         <UserButton />
      </SignedIn>
-     
-    </div>
+      
+    </header>
   )
 }
 
