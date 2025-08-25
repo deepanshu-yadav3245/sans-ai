237877c12c876a6e96e0ff03c1a93b1b56c5146b
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-25 22:42:20 +0530

    UPLOAD

--- components/hearder.jsx
+++ components/hearder.jsx
@@ -1,6 +1,8 @@
 import { SignedIn, SignedOut,SignInButton,UserButton } from "@clerk/nextjs"
 import Image from "next/image"
 import Link from "next/link"
+import { Button } from "./ui/button"
+import { LayoutDashboard } from "lucide-react"
 const Header = () => {
   return (
     <header>
@@ -15,6 +17,16 @@ const Header = () => {
           className="h-12 py-1 w-auto  object-contain"/>
           
         </Link>
+        <div>
+           <SignedIn>
+               <Link href={'/dashboard'}>
+                  <Button>
+                     <LayoutDashboard className="h-4 w-4 "/>
+                     Industry Insights
+                  </Button>
+               </Link>
+           </SignedIn>
+        </div>
       </nav>
 
      <SignedOut>
