12c672b0ffb631479681cf9005a7fe733ce3d831
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-27 14:01:06 +0530

    upload

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-
+import { Button } from './ui/button'
+import Link from 'next/link'
 const HeroSection = () => {
   return (
     <section className='w-full pt-36 md:pt-48 pb-10'>
@@ -13,6 +14,18 @@ const HeroSection = () => {
                 Advance your career with personalized AI-driven coaching, tailored 
                 to your unique professional journey.
             </p>
+        </div>
+
+        <div>
+            <Link href='/dashboard'>
+               <Button size="lg" className="px-8">
+                Get Started</Button>
+            </Link>
+
+            <Link href='https://www.youtube.com/watch?v=UbXpRv5ApKA&t=145s'>
+               <Button size="lg" className="px-8" variant="outline">
+                Get Started</Button>
+            </Link>
         </div>
       </div>
     </section>
