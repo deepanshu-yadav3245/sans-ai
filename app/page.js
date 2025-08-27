eba67a13e9f2155bc1c28ee2b3de97cdb033c60b
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-27 13:28:16 +0530

    upload

--- app/page.js
+++ app/page.js
@@ -4,9 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return(
    <div>
-    Hi My Name is Rwady Rahul
-    <br/>
-    <Button>Hello</Button>
+   
     </div>
   )
 }
