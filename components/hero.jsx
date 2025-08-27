1bbc4a3c9489620f1975f245922ab670d5cae7c7
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-27 13:56:24 +0530

    upload

--- components/hero.jsx
+++ components/hero.jsx
@@ -2,8 +2,19 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-    <section>
-      
+    <section className='w-full pt-36 md:pt-48 pb-10'>
+      <div>
+        <div>
+            <h1>Your AI Coach for
+                <br />
+                Professional Success
+            </h1>
+            <p>
+                Advance your career with personalized AI-driven coaching, tailored 
+                to your unique professional journey.
+            </p>
+        </div>
+      </div>
     </section>
   )
 }
