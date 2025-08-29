5ecfdae945f63bca02ed8141cf32ffdc3644f2d6
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-29 22:51:46 +0530

    upload

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,22 @@
 import { Button } from "@/components/ui/button";
 import HeroSection from "@/components/hero";
 import { features } from "@/data/features";
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,CardAction } from "@/components/ui/card"; 
+import { Card, CardContent, } from "@/components/ui/card"; 
 
 export default function Home() {
   return(
    <div>
-       <div className="grid-background">
+       <div className="grid-background"> </div>
         <HeroSection />
 
         <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
            <div className="container mx-auto px-4 md:px-6">
-              <h2 className="text-3xl font-bold tracking-tighter text-center md-12">PowerFull Features for Your Career Growth</h2>
+              <h2 className="text-3xl font-bold tracking-tighter text-center mb-12">PowerFull Features for Your Career Growth</h2>
               <div
               className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto"
               >{features.map((features,index)=>{
                 return(
-              <Card key={index}>
+              <Card key={index} className="border-2 hover:border-primary transition-colors duration-300">
                    
                    <CardContent>
                        <div>
@@ -32,7 +32,7 @@ export default function Home() {
            </div>
         </section>
 
-       </div>
+      
     </div>
   )
 }
