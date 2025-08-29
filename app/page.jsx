bdf352a72375526d436d4dbc67c4516631b22305
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-29 23:54:57 +0530

    upload

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import HeroSection from "@/components/hero";
 import { features } from "@/data/features";
 import { Card, CardContent, } from "@/components/ui/card"; 
+import { howItWorks } from "@/data/howItWorks";
 
 export default function Home() {
   return(
@@ -52,6 +53,31 @@ export default function Home() {
                   <p className="text-muted-foreground">AI Support</p>
                 </div>
               </div>
+           </div>
+        </section>
+
+        
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+           <div className="container mx-auto px-4 md:px-6">
+            <div className="text-center max-w-3xl mx-auto mb-12">
+               <h2 className="text-3xl font-bold mb-4 ">How Its Work</h2>
+              <p className="text-muted-foreground">Four Simple steps to Accelerate your career growth</p>
+            </div>
+              
+              <div
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto"
+              >{howItWorks.map((item,index)=>{
+                return (
+                  <div key={index} className="flex flex-col items-center text-center space-y-4">
+                     <div className="w-16 h-16 rounded-full bg-primary/10 flex items-center justify-center">
+                       {item.icon}
+                     </div>
+                     <h3 className="font-semibold text-xl">{item.title}</h3>
+                     <p className="text-muted-foreground">{item.description}</p>
+                  </div>
+                )
+              })}
+             </div>
            </div>
         </section>
       
