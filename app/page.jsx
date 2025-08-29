3db367dd52883620be2df8f3df96ddf6846aab85
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-08-29 22:41:50 +0530

    upload

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import HeroSection from "@/components/hero";
 import { features } from "@/data/features";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle,CardAction } from "@/components/ui/card"; 
 
 export default function Home() {
   return(
@@ -8,24 +9,24 @@ export default function Home() {
        <div className="grid-background">
         <HeroSection />
 
-        <section>
-           <div>
-              <h2>PowerFull Features for Your Career Growth</h2>
-              <div>{features.map((features,index)=>{
+        <section className="w-full py-12 md:py-24 lg:py-32 bg-background">
+           <div className="container mx-auto px-4 md:px-6">
+              <h2 className="text-3xl font-bold tracking-tighter text-center md-12">PowerFull Features for Your Career Growth</h2>
+              <div
+              className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto"
+              >{features.map((features,index)=>{
                 return(
-                  <Card>
-                    <CardHeader>
-                        <CardTitle>Card Title</CardTitle>
-                        <CardDescription>Card Description</CardDescription>
-              <CardAction>Card Action</CardAction>
-            </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-  <CardFooter>
-    <p>Card Footer</p>
-  </CardFooter>
-</Card> 
+              <Card key={index}>
+                   
+                   <CardContent>
+                       <div>
+                         {features.icon}
+                         <h3>{features.title}</h3>
+                         <p>{features.description}</p>
+                       </div>
+                   </CardContent>
+                  
+             </Card> 
                 )
               })}</div>
            </div>
