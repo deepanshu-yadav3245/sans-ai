f32f01d4516fe5f9abc5be8e9698dd5beecd6d95
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-01 00:19:43 +0530

    upload

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 const MainLayout = ({ children }) => {
+    // Redirect user after onboarding
   return (
     <div className='container mx-auto mt-24 mb-20'>
       {children}
