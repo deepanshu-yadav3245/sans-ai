47f40953aaf5666a1dbab1c488039f9c15ac0317
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-01 18:38:58 +0530

    upload

--- actions/user.js
+++ actions/user.js
@@ -3,23 +3,59 @@
 import { auth } from "@clerk/nextjs/server";
 import {db} from "@/lib/prisma";
 
+
 export async function updateUser(data) {
-    const {userid} = await auth();
-    if (!userid) throw new Error("User not authenticated");
+    const {userId} = await auth();
+    if (!userId) throw new Error("User not authenticated");
 
     const user = await db.user.findUnique({ 
         where:{
-            clerkUserId: userid
+            clerkUserId: userId,
         },
     });
 
     if (!user) throw new Error("User not found");
 
     try {
-         const result = await db.$transaction(async () => {
+         const result = await db.$transaction(
+            async (tx) => {
               // find if the industry exits
+              let industryInsight = await tx.industryInsight.findUnique({
+                where:{
+                    industry:data.industry
+                },
+              })
              // if industry doesn't exits,create it with default vlaues - will replace it with ai later 
+             if (!industryInsight){
+                industryInsight = await tx.industryInsight.create({
+                    data: {
+                        industry:data.industry,
+                        salaryRanges:[], // default value
+                        growthRange:0, // default value
+                        demandLevel:"Medium", // Default value
+                        topSkill:[], // Default empty Array
+                        marketOutLook:"Neutral", // Default value
+                        keyTreands:[],
+                        recommendedSkills:[],
+                        nextUpdate:new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 1 week from now
+
+                    }
+                })
+             }
              // update the user  
+             const updatedUser =  await tx.user.update({
+                where: {
+                    id:user.id,
+                },
+                data: {
+                    industry:data.industry,
+                    experience:data.experience,
+                    bio:data.bio,
+                    skills:data.skills
+                },
+             })
+
+             return {updateUser,industryInsight}
          },
          {
             timeout:10000, // default:5000
@@ -31,5 +67,31 @@ export async function updateUser(data) {
     } catch (error) {
         console.error("Error updating user and industry:", error.message);
         throw new Error("Failed to update profile");
+    }
+}
+
+export async function getUserOnboardingStatus(){
+    const {userId} = await auth();
+    if (!userId) throw new Error("User not authenticated");
+
+    const user = await db.user.findUnique({ 
+        where:{
+            clerkUserId: userId,
+        },
+    });
+
+    if (!user) throw new Error("User not found");
+
+    try {
+        const user = await db.user.findUnique({
+            where: {
+                clerkUserId:userId,
+            },
+            select: {
+                industry:true
+            },
+        })
+    } catch (error){
+
     }
 }
