8445facb8b3fd1abdeddcad5e7f459c7bb648f70
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-04 18:11:50 +0530

    upload

--- actions/interview.js
+++ actions/interview.js
@@ -1,5 +1,15 @@
 "use server";
+import {db} from "@/lib/prisma"
+import {auth} from "@clerk/nextjs/server"
 
 export async function generateQuize(){
+     const {userId} = await auth();
+        if (!userId) throw new Error("User not authenticated");
+    
+        const user = await db.user.findUnique({ 
+            where:{
+                clerkUserId: userId,
+            },
+        });
     
 }
