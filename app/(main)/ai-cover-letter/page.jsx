589ada2c382a53a8d7e1ce0697e46e80dd847af4
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-06 16:21:12 +0530

    update

--- app/(main)/ai-cover-letter/page.jsx
+++ app/(main)/ai-cover-letter/page.jsx
@@ -11,7 +11,7 @@ export default async function CoverLetterPage() {
     <div>
       <div className="flex flex-col md:flex-row gap-2 items-center justify-between mb-5">
         <h1 className="text-6xl font-bold gradient-title">My Cover Letters</h1>
-        <Link href="/ai-cover-letter/new">
+        <Link href="/ai-cover-letter/news">
           <Button>
             <Plus className="h-4 w-4 mr-2" />
             Create New
