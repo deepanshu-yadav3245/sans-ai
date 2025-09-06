30f0c4df3466d42885cc4523f5af1921333b71b6
deepanshuyadav <162795413+deepanshu-yadav3245@users.noreply.github.com>
2025-09-06 16:20:46 +0530

    update

--- app/(main)/ai-cover-letter/page.jsx
+++ app/(main)/ai-cover-letter/page.jsx
@@ -14,7 +14,7 @@ export default async function CoverLetterPage() {
         <Link href="/ai-cover-letter/new">
           <Button>
             <Plus className="h-4 w-4 mr-2" />
-            Create New
+            Create News
           </Button>
         </Link>
       </div>
